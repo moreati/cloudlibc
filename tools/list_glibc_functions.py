0a1dc8f1ee63a8dc1b7a5c87d0216d6aa368d0c4
Alex Willmer <alex@moreati.org.uk>
2016-07-07 20:29:15 +0100

    Encapsulate script as a function

--- tools/list_glibc_functions.py
+++ tools/list_glibc_functions.py
@@ -4,11 +4,15 @@ import urllib.request
 import re
 
 
-with urllib.request.urlopen('https://www.gnu.org/software/libc/manual/html_node/Function-Index.html') as f:
-    s = f.read().decode('utf-8')
+def functions():
+    with urllib.request.urlopen('https://www.gnu.org/software/libc/manual/html_node/Function-Index.html') as f:
+        s = f.read().decode('utf-8')
 
-function_names = [name.lstrip('*') for name in re.findall('<code>(.+?)</code>', s)]
-function_names.sort()
+    function_names = [name.lstrip('*') for name in re.findall('<code>(.+?)</code>', s)]
+    function_names.sort()
+    return function_names
 
-for name in function_names:
-    print(name)
+if __name__ == '__main__':
+    function_names = functions()
+    for name in function_names:
+        print(name)
