484f93543949d091677005efe511ef798ca7a3e0
Alex Willmer <alex@moreati.org.uk>
2016-07-07 20:28:06 +0100

    Read symbols from the file provided

--- tools/list_cloudlibc_functions.py
+++ tools/list_cloudlibc_functions.py
@@ -1,37 +1,16 @@
 #!/usr/bin/env python3
 
 import os
-import re
 
-def fpaths(path):
-    for base, dnames, fnames in os.walk(path):
-        for fname in fnames:
-            yield os.path.join(base, fname)
+def functions():
+    symbols = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                           '../src/libc/symbols'))
+    with open(symbols) as f:
+        function_names = [line.rstrip() for line in f]
+    function_names.sort()
+    return function_names
 
-def decls(path):
-    with open(path, encoding='utf-8') as f:
-        s = f.read()
-        decls = re.findall(r'__BEGIN_DECLS\n(.+?)__END_DECLS', s, re.DOTALL)
-    return decls
-
-def functions(decl):
-    decl = decl.rstrip('\n;')
-    logical_lines = [s.strip() for s in decl.split(';')]
-    functions = [line for line in logical_lines
-                 if not line.startswith(('#define','_MACRO'))
-                    and line.endswith(')')]
-    return [re.sub(r'[ \t\n]+', ' ', fn) for fn in functions]
-
-def function_name(func):
-    l, r = func.split('(', 1)
-    rtype, name = l.rsplit(' ', 1)
-    return name.lstrip('*')
-
-function_names = [function_name(func) for path in fpaths('src/include')
-                                      for decl in decls(path)
-                                      for func in functions(decl)]
-
-function_names.sort()
-
-for function_name in function_names:
-    print(function_name)
+if __name__ == '__main__':
+    function_names = functions()
+    for function_name in function_names:
+        print(function_name)
